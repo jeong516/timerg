beb39fcc626b8a76a6bbd696836273d0751de065
jeong516 <50651709+jeong516@users.noreply.github.com>
2025-09-05 23:04:54 +0900

    Use in-article

--- sec/script.js
+++ sec/script.js
@@ -48,7 +48,8 @@ document.addEventListener('DOMContentLoaded', () => {
         let adCnt = 0;
         // var adSlots = ['9913237084', '3806993730', '2493912060', '1180830393'];
         // var adSlots = ['7733796305', '7733796305', '7733796305', '7733796305'];
-        var adSlots = ['5434543472', '5434543472', '5434543472', '5434543472'];
+        // var adSlots = ['5434543472', '5434543472', '5434543472', '5434543472'];
+        var adSlots = ['5716167978', '5716167978', '5716167978', '5716167978'];
         for (var i = 0; i < froLen && i < postsToDisplay.length; i++) {
             post = postsToDisplay[i];
 
@@ -86,7 +87,8 @@ document.addEventListener('DOMContentLoaded', () => {
                     adCnt++;
                     // adDiv.innerHTML = '<ins class="adsbygoogle" style="display:block; width: 100%;" data-ad-client="ca-pub-6836676689902404" data-ad-slot="' + adSlot + '" data-ad-format="auto" data-full-width-responsive="true"></ins>';
                     // adDiv.innerHTML = '<ins class="adsbygoogle" style="display:block; width: 100%;" data-ad-format="fluid" data-ad-layout-key="-fb+5w+4e-db+86" data-ad-client="ca-pub-6836676689902404" data-ad-slot="' + adSlot + '"></ins>';
-                    adDiv.innerHTML = '<ins class="adsbygoogle" style="display:block; width: 100%;" data-ad-format="autorelaxed" data-ad-client="ca-pub-6836676689902404" data-ad-slot="' + adSlot + '"></ins>';
+                    // adDiv.innerHTML = '<ins class="adsbygoogle" style="display:block; width: 100%;" data-ad-format="autorelaxed" data-ad-client="ca-pub-6836676689902404" data-ad-slot="' + adSlot + '"></ins>';
+                    adDiv.innerHTML = '<ins class="adsbygoogle" style="display:block; width: 100%;" data-ad-format="in-article" data-ad-format="fluid" data-ad-client="ca-pub-6836676689902404" data-ad-slot="' + adSlot + '"></ins>';
                     postsContainer.appendChild(adDiv);
                     adsList.push(adDiv);
                     (adsbygoogle = window.adsbygoogle || []).push({});
